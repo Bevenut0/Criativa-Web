cd073ba09431b0dad78d2e657203a350f8906150
victor <victorgabrielbevenuta@gmail.com>
2026-01-17 15:44:24 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const boxes = document.getElementsByClassName('box');
+
+
+for (let box of boxes) {
+    box.addEventListener('click', () => {
+        const text = document.getElementById("servicos-text");
+        text.style.fontSize = "18px"
+        text.style.fontStyle = "italic"
+        if(box == boxes[0]) {
+            text.innerHTML = "Desing de Web";
+        } else if(box == boxes[1]) {
+            text.innerHTML = "Desing Grafico";
+        } else if(box == boxes[2]) {
+            text.innerHTML = "Desing de Moda";
+        } else if(box == boxes[3]){
+            text.innerHTML = "Desing de Luxo";
+        };
+    });
+};
